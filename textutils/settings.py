01ca964de7d44b4c4d05c68cc275ef0f31757e5a
Kshitiz Raj <kshitiz2228@gmail.com>
2021-05-18 18:24:46 +0530

    Version 1.4

--- textutils/settings.py
+++ textutils/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-%9&#qb$=gy%rufq_=pi!($xcrm^63utg&34ku761ghkirlv%gu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',cast=bool, default=True)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
